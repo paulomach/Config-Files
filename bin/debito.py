5a7d268dd609dffd9c6af3c5bc05aa1862d7851f
Paulo Machado <paulo.silveiramachado@fit-tecnologia.org.br>
2016-06-13 13:55:21 -0300

    using ibge sidra api (json/rest)

--- bin/debito.py
+++ bin/debito.py
@@ -7,7 +7,8 @@
 # WARNING! All changes made in this file will be lost!
 
 from PyQt5 import QtCore, QtWidgets
-from lxml import html
+from datetime import date
+from collections import OrderedDict
 import requests
 
 class Ui_MainWindow(object):
@@ -57,51 +58,27 @@ class Ui_MainWindow(object):
     def dataRetrieve(self):
         self.plainTextEdit.clear()
         self.plainTextEdit.appendPlainText("retrieving data from source...")
-        # http://www.sidra.ibge.gov.br/AjudaAPISidra.htm
-        # http://www.sidra.ibge.gov.br/bda/tabela/protabl.asp?c=1737&z=ia&o=3&i=P
-        # http://www.sidra.ibge.gov.br/api/values/t/1737/p/201509-201605/v/63/n1/1/h/n/f/c
-        url = "http://www.ibge.gov.br/home/estatistica/indicadores/precos/inpc_ipca/ipca-inpc_201604_1.shtm"
-        page = requests.get(url)
-        tree = html.fromstring(page.content)
-        table = tree.xpath('//table[@summary="Tabela com série histórica IPCA"]/thead/tr/td')
-        j = 0
-        k = 0
-        ipca = [];
-        for i in table:
-            value = i.text_content().strip()
-            if j == 1:
-                if value == "Set":
-                    j = 2
-            if j == 2:
-                if value.isalpha():
-                    k = 0
-                k += 1
-                if k == 3:
-                    ipca.append(float(value.replace(',','.')))
-            if value == "2015":
-                j = 1
-        #        9,  10,11,12   1,2,3,4
-        drawee = [70000,0,0,10000,0,0,0,31500,31500]
-        dates = ["21/09/2015", "21/12/2015", "20/04/2016","20/05/2016"]
-        k = 0
-        leftover = 0
+        url = "http://www.sidra.ibge.gov.br/api/values/t/1737/p/201509-201704/v/63/n1/1/h/n/f/c"
+        ipca = requests.get(url).json()
+        # script updatable 
+        drawee = {'201509':70000, '201512':10000, '201604':31500, '201605':31500}
+        drawee = OrderedDict(sorted(drawee.items()))
+        
         self.plainTextEdit.clear()
         self.plainTextEdit.appendPlainText(
             "Data;Retirada;IPCA acumulado;Corrigido;Saldo")
-        for i in range(0,len(drawee)):
-            if drawee[i] > 0:
-                interest = 1
-                for j in range(i,len(ipca)-1):
-                    interest = interest*(1+ipca[j]/100)
-                accumulated = round((interest-1), 4)
-                corrected = round(drawee[i]*interest, 2)
-                leftover = round(corrected + leftover, 2)
-                text = dates[k] + ";" + str(drawee[i]) + ";" + str(accumulated).replace('.', ',')\
-                + ";" + str(corrected).replace('.', ',') + ";" + str(leftover).replace('.', ',')
-                self.plainTextEdit.appendPlainText(text)
-                k += 1
-
-
+        
+        vtotal=0
+        for d, v in drawee.items():
+            index = 1
+            for i in range(0, len(ipca)):
+                if ipca[i].get('D1C') == d:
+                    break
+            for j in range(i, len(ipca)):
+                index = round(index*(1+ float(ipca[j].get('V'))/100), 4)
+            vtotal = round(vtotal+v*index, 2)
+            line = d + ";" + str(v) + ";" + str(index) + ";" + str(round(v*index, 2)) + ";" + str(vtotal)
+            self.plainTextEdit.appendPlainText(line)
 
 if __name__ == "__main__":
     import sys
@@ -111,4 +88,3 @@ if __name__ == "__main__":
     ui.setupUi(MainWindow)
     MainWindow.show()
     sys.exit(app.exec_())
-
